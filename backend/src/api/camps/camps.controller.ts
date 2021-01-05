b6559722dadc2aec440ff15cc3c705148cd017d9
homfarnam <homfarnam@gmail.com>
2021-01-05 23:10:47 +0330

    Update camp controller

--- backend/src/api/camps/camps.controller.ts
+++ backend/src/api/camps/camps.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { CampsService } from './camps.service';
+import { CreateCampDto } from './dto/create-camp.dto';
+import { UpdateCampDto } from './dto/update-camp.dto';
 
 @Controller('camps')
-export class CampsController {}
+export class CampsController {
+  constructor(private readonly campService: CampsService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.campService.findAllCamps(paginationQuery);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.campService.findOneCamp(id);
+  }
+
+  @Post()
+  create(@Body() createCampDto: CreateCampDto) {
+    console.log(createCampDto instanceof CreateCampDto);
+    return this.campService.createCamp(createCampDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCampDto: UpdateCampDto) {
+    return this.campService.updateCamp(id, updateCampDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.campService.removeCamp(id);
+  }
+}
