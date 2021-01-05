745b4d3ea331871fce942ac543dba521b023ae86
homfarnam <homfarnam@gmail.com>
2021-01-05 23:11:20 +0330

    update camps module

--- backend/src/api/camps/camps.module.ts
+++ backend/src/api/camps/camps.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Camp, CampSchema } from 'src/common/mongo';
 import { CampsController } from './camps.controller';
 import { CampsService } from './camps.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Camp.name,
+        schema: CampSchema,
+      },
+    ]),
+  ],
   controllers: [CampsController],
   providers: [CampsService],
 })
